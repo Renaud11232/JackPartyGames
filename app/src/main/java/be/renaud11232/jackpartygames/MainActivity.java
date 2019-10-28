d2da08c7d45c3575535b8327550e0c2532ddef55
Renaud Gaspard <gaspardrenaud@hotmail.com>
2019-10-28 13:35:57 +0100

    added delicious cookies

--- app/src/main/java/be/renaud11232/jackpartygames/MainActivity.java
+++ app/src/main/java/be/renaud11232/jackpartygames/MainActivity.java
@@ -3,7 +3,9 @@ package be.renaud11232.jackpartygames;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -20,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient(new CustomWebViewClient());
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        if(Build.VERSION.SDK_INT >= 21){
+            cookieManager.setAcceptThirdPartyCookies(webView, true);
+        }
         webView.loadUrl(getString(R.string.url_homepage));
     }
 }
